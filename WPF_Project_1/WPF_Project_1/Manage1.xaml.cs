b707565e4153e9b6229f5cfd6891e518709acc92
Wayrent <sebu1404@gmail.com>
2024-02-14 12:15:13 +0300

    Added Manage Window
    Hz rabotaet ili net, no voobsche dolzhno

---
+++ WPF_Project_1/WPF_Project_1/Manage1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Project_1
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Manage1 : Window
+    {
+        public Manage1()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            // Логика удаления сотрудника
+        }
+
+        private void AddEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            // Логика добавления нового сотрудника
+        }
+    }
+}
